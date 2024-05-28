3d1a4118906a5d2a5965016bfd133272deae9747
sandeepsingh2383@gmail.com <sandeepsingh2383@gmail.com>
2024-05-28 10:34:34 +0530

    add video

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <h4>Welcome to Video engage app</h4>
       </header>
+      <div className="video-container">
+        <div className="voomly-embed player-styles" 
+          data-id="JGQueDo_w" 
+          data-ratio="1.000000" 
+          data-type="f" 
+          data-skin-color="#008EFF" 
+          data-shadow="">
+            
+          </div>
+        </div>
     </div>
   );
 }
